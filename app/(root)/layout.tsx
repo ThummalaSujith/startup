140b137d62ed4e8eb3608ba0537362c90696d34f
Thummala Sujith <srt7@njit.edu>
2025-02-28 10:50:22 -0500

    Created nextjs Form

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
-export default function Layout({children}:Readonly<{children:React.ReactNode}>){
-return (
-    <main>
+import Navbar from "../components/Navbar";
 
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
 
+      {children}
     </main>
-)
+  );
 }
